9e08b852814ddba1ccc5761148533ad2094cbaaa
TahirHSTU <45144696+TahirHSTU@users.noreply.github.com>
2021-01-31 12:03:23 +0600

    app completed with security patched

--- todo-app/server.js
+++ todo-app/server.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let mongodb = require('mongodb')
+let sanitizeHTML = require('sanitize-html')
 
 let app = express()
 let db
@@ -17,6 +18,19 @@ useUnifiedTopology: true}, function(err, client){
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+function passwordProtected(request, response, next) {
+  response.set('WWW-Authenticate','Basic realm= "Simple Todo App"')
+  console.log(request.headers.authorization)
+  if(request.headers.authorization == "Basic dG9kb2FwcDp0b2RvYXBw") {
+    next()
+
+  } else {
+    response.status(401).send("Äuthentication required")
+  }
+}
+
+app.use(passwordProtected)
+
 app.get('/', function(request, response) {
   db.collection('items').find().toArray(function(err, items) {
     response.send(`<!DOCTYPE html>
@@ -32,27 +46,22 @@ app.get('/', function(request, response) {
         <h1 class="display-4 text-center py-1">To-Do App!</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form action="/create-item" method="POST">
+          <form id="create-form" action="/create-item" method="POST">
             <div class="d-flex align-items-center">
-              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
         </div>
         
-        <ul class="list-group pb-5">
-          ${items.map(function(item) {
-            return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">${item.text}</span>
-            <div>
-              <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>`
-          }).join('')}
+        <ul id="item-list" class="list-group pb-5">
+          
         </ul>
         
       </div>
+      <script>
+      let items = ${JSON.stringify(items)}
+      </script>
       <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
       <script src="/browser.js"></script>  
@@ -65,9 +74,10 @@ app.get('/', function(request, response) {
 
 app.post('/create-item', function(request, response){
 //  console.log(request.body.item)
-db.collection('items').insertOne({text: request.body.item}, function(){
+let safeText = sanitizeHTML(request.body.text, {allowedTags: [], allowedAttributes: {}})
+db.collection('items').insertOne({text: safeText}, function(err, info){
   // response.send("Thanks for submitting the form")
-  response.redirect('/')
+  response.json(info.ops[0])
 
 })
 })
@@ -76,9 +86,16 @@ db.collection('items').insertOne({text: request.body.item}, function(){
 app.post('/update-item', function(request, response){
   // console.log(request.body.text)
   // response.send("success")
-  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectID(request.body.id)}, {$set: {text: request.body.text}}, function(){
+  let safeText = sanitizeHTML(request.body.text, {allowedTags: [], allowedAttributes: {}})
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectID(request.body.id)}, {$set: {text: safeText}}, function(){
     response.send("success.")
   })
 })
 
+
+app.post('/delete-item', function(request, response){
+db.collection("items").deleteOne({_id: new mongodb.ObjectID(request.body.id)}, function() {
+  response.send("success")
+})
+})
 
